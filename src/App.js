9d09e2b0523e7ced97082d8695803bd23c7775a2
DanielMa <mwldaniel_008@126.com>
2022-11-24 18:24:28 +0800

    Llifecycle Method: componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -7,31 +6,35 @@ class App extends Component {
     super()
 
     this.state = {
-      name: { firstName: 'Yihua', lastName: 'Zhang' },
-      company: 'ZTM',
+      monsters: [],
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{' '}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(() => {
-                return {
-                  name: { firstName: 'Andrei', lastName: 'Neaogie' },
-                }
-              })
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
       </div>
     )
   }
