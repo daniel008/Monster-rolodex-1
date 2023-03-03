1783b6a8029040d3dcb2699bae4ee9534e165a65
DanielMa <mwldaniel_008@126.com>
2023-03-03 10:15:05 +0800

    typing react's setState understanding generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
 import './App.css'
 import CardList from './components/card-list/card-list.compoent'
 import SearchBox from './components/search-box/search-box.component'
+import { getData } from './utils/data.utils'
+
+export type Monster = {
+  id: string
+  name: string
+  email: string
+}
 
 const App = () => {
   console.log('render')
   const [searchField, setSearchField] = useState('')
-  const [monsters, setMonsters] = useState([])
+  const [monsters, setMonsters] = useState<Monster[]>([])
   const [filteredMonsters, setFilteredMonsters] = useState(monsters)
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users))
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      setMonsters(users)
+    }
+    fetchUsers()
   }, [])
 
   useEffect(() => {
@@ -25,7 +36,7 @@ const App = () => {
     console.log('effect is firing')
   }, [monsters, searchField])
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase()
     setSearchField(searchFieldString)
   }
